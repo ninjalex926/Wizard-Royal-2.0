928435faa48a6f3d7bd467e31ad1a72d847c2645
ninjalex926 <45501273+ninjalex926@users.noreply.github.com>
2021-08-26 14:56:48 -0500

    Orginize Project 4
    Partcles orgrinzed

---
+++ Assets/Scripts/WinLoseCon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLoseCon : MonoBehaviour
+{
+  public  ABC_StateManager player_ABCSM;
+
+  public ABC_StateManager enemy_ABCSM;
+
+   
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if(player_ABCSM.currentHealth <= 0)
+        {
+            print("YOU LOSE");
+        }
+
+      
+
+        if (enemy_ABCSM.currentHealth <= 0)
+        {
+            print("YOU Win");
+        }
+
+
+       //    print(enemy_ABCSM.currentHealth);
+
+     //    print(player_ABCSM.healthABC);
+    }
+}
