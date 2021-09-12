9271e3ce11cea2047cea70c9b5eb5f1f518dc151
ninjalex926 <45501273+ninjalex926@users.noreply.github.com>
2021-09-12 17:36:18 -0500

    better arena

---
+++ Assets/Scripts/ABCUI_SpellController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABCUI_SpellController : MonoBehaviour
+{
+
+
+
+    public GameObject playerRef;
+
+    public GameObject UI_BookSpell_Icon;
+
+    public ABC_Controller abcCon;
+
+    public ABC_Ability nullbullet;
+
+     int nullboltID = 1123773;
+
+    public string nullbulletStr;
+
+    public bool isAbilityEnabled;
+
+  //  public ABC_IEntity abcIE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+
+        abcCon = playerRef.GetComponent<ABC_Controller>();
+
+        nullbullet = abcCon.FindAbility(1123773);
+
+        nullbullet.abilityEnabled = true;
+
+
+
+        //  print(nullbullet.name);
+
+    }
+}
---
+++ Assets/Scripts/EnemyWaveManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using UnityEngine.UI;
+
+public class EnemyWaveManager : MonoBehaviour
+{
+    public ABC_StateManager player_ABCSM;
+
+    // public ABC_StateManager enemy_ABCSM;
+
+    public bool battleActive;
+
+  //  public GameObject[] enemyGroups;
+
+    // The current number of waves
+    public int waveNum;
+
+    // How many enmies need to be killed to win
+    public int waveKCGoal;
+
+    // How many emies have been killed
+    public int currentKC;
+
+    // How many enemies are currently in the map
+    public int activeEnemiesNum;
+
+    public GameObject[] activeEnemies;
+
+    public Text waveNumText;
+
+    public Text waveGoalText;
+
+    public GameObject winText;
+
+    public GameObject loseText;
+
+    public void Start()
+    {
+        //  Get the number of Active aenmies
+        activeEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in activeEnemies)
+        {
+            activeEnemiesNum += 1;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+        waveNumText.text = ("Wave:" + "" + waveNum);
+
+        waveGoalText.text = ("Kill Count" + "" + currentKC + "/" + waveKCGoal);
+
+
+
+        // If Player Health reaches 0, You lose
+        if (player_ABCSM.currentHealth <= 0)
+        {
+            print("YOU LOSE");
+
+            loseText.SetActive(true);
+
+            //   SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+        // if Player kills amount needed to wun the wave
+        if(currentKC >= waveKCGoal)
+        {
+            winText.SetActive(true);
+
+            print("YOU Win");
+        }
+
+
+    }
+
+   public void IncreaseKC()
+    {
+        ++currentKC;
+    }
+
+}
